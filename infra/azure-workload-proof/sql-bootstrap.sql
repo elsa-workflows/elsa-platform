803315df0f318b76e79d0366a63415c6258bf98b
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2026-08-29 12:07:06 +0200

    Correct Azure SQL service principal SID encoding

--- infra/azure-workload-proof/sql-bootstrap.sql
+++ infra/azure-workload-proof/sql-bootstrap.sql
@@ -10,7 +10,8 @@ IF NOT EXISTS (SELECT 1 FROM sys.database_principals WHERE name = N'__WORKLOAD_I
 BEGIN
     -- A service-principal contained user uses the application's client ID as
     -- its SID. This avoids a directory lookup during bootstrap.
-    DECLARE @sid VARBINARY(16) = CONVERT(VARBINARY(16), REPLACE('__WORKLOAD_IDENTITY_CLIENT_ID__', '-', ''), 2);
+    DECLARE @clientId UNIQUEIDENTIFIER = '__WORKLOAD_IDENTITY_CLIENT_ID__';
+    DECLARE @sid VARBINARY(16) = CONVERT(VARBINARY(16), @clientId);
     DECLARE @sidHex NVARCHAR(34) = CONVERT(NVARCHAR(34), @sid, 1);
     EXEC (N'CREATE USER [__WORKLOAD_IDENTITY_NAME__] WITH SID = ' + @sidHex + N', TYPE = E');
 END;
